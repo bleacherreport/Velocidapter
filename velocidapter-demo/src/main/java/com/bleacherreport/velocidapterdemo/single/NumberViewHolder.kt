46ae1023d3f65cfe6070fb49eeb53ea70bd531ab
Brad Thome <brad.thome1@gmail.com>
2021-04-06 11:51:33 -0500

    update ReadMe

--- velocidapter-demo/src/main/java/com/bleacherreport/velocidapterdemo/single/NumberViewHolder.kt
+++ velocidapter-demo/src/main/java/com/bleacherreport/velocidapterdemo/single/NumberViewHolder.kt
@@ -6,11 +6,13 @@ import com.bleacherreport.velocidapterannotations.ViewHolder
 import com.bleacherreport.velocidapterdemo.MainActivity
 import com.bleacherreport.velocidapterdemo.databinding.ItemNumberBinding
 
+/** ViewBinding Top Level Extension Function **/
 @ViewHolder(adapters = [MainActivity.SingleAdapter, MainActivity.MultiAdapter])
 fun ItemNumberBinding.bind(item: NumberViewItemBindingExtension) {
     textView.text = item.text
 }
 
+/** ViewBinding Object Member Extension Function **/
 object Test {
     @ViewHolder(adapters = [MainActivity.SingleAdapter, MainActivity.MultiAdapter])
     fun ItemNumberBinding.bindTest(item: NumberViewItemBindingMemberFunction) {
@@ -18,9 +20,9 @@ object Test {
     }
 }
 
+/** ViewBinding ViewHolder Class **/
 @ViewHolder(adapters = [MainActivity.SingleAdapter, MainActivity.MultiAdapter])
 class NumberViewHolder(val binding: ItemNumberBinding) : RecyclerView.ViewHolder(binding.root) {
-
     @Bind
     fun bind(item: NumberViewItemViewHolder, position: Int) {
         binding.bind(item.number.copy(text = item.number.text + " @ position $position"))
